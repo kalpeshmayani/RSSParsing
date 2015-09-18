b240bf6cbf2d2679eeca45f0caaf40dd0c50b5d5
kalpeshmayani <kalpeshdmayani1990@gmail.com>
2015-09-18 17:11:49 +0530

    RssParsing in Android App
    RssParsing in Android App

--- src/com/kpinfotech/global/AppConstant.java
+++ src/com/kpinfotech/global/AppConstant.java
@@ -2,6 +2,7 @@ package com.kpinfotech.global;
 
 public class AppConstant {
 	
+	
     // regular expressions
 
 	public static final String EMAIL_EXPRESS = "^[A-Za-z0-9]+([\\.\\-_]{1}[A-Za-z0-9]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,4})$";
